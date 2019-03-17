0c032aca05db7303ca67b0162d7c032049199cd1
wanjun <wanjuncode@163.com>
2019-03-18 00:47:37 +0800

    修改了代码格式

--- main/client_test.cpp
+++ main/client_test.cpp
@@ -9,17 +9,14 @@
 #include <stdint.h>
 #include <string.h> // bzero
 #include <stdlib.h>
+#include <thread>
 #include <pthread.h>
 
 #include "shm_mutex.h"
-
 #include "../common/nginx/nginx_log.h"
 #include "../common/net/net_tool.h"
 
-#include <thread>
 using namespace NET;
-
-int listenfd;
 
 // 对应 libevent server 中 echo package 包格式
 typedef struct message
@@ -30,8 +27,13 @@ typedef struct message
 
 size_t bufferSize = 1024;
 
-void *receive_loop(void *args)
+void cleanup_method(void *args){
+    printf("this is receive loop thread's cleanup mothed\n");
+}
+
+void *async_receive_thread(void *args)
 {
+    pthread_cleanup_push(cleanup_method,NULL);
     remote_client *client = (remote_client *)(args);
     char buffer[bufferSize];
     bzero(buffer, bufferSize);
@@ -42,10 +44,11 @@ void *receive_loop(void *args)
         auto length = recv(client->clientfd, buffer, bufferSize, 0);
         printf("receive[%lu] :[%s]\n", length, buffer);
         bzero(buffer, bufferSize);
-        // if (0 == strncmp(buffer, "return", strlen("return")))
-        //     break;
     }
+    // never be here
     printf("out of receive loop\n");
+    pthread_cleanup_pop(1);
+    return NULL;
 }
 
 void client_to_server()
@@ -61,9 +64,8 @@ void client_to_server()
         return;
     }
 
-    // std::thread receive(receive_loop, std::ref(client),60);
     pthread_t receiveThread;
-    pthread_create(&receiveThread,NULL,receive_loop,&client);
+    pthread_create(&receiveThread,NULL,async_receive_thread,&client);
     while (true)
     {
         std::string message;
@@ -93,9 +95,86 @@ void client_to_server()
         write(client.clientfd, sendMessage.data(), sendMessage.length());
     }
     
+    // 使用 pthread_cancel 是会调用 pthread_cleanup mothed 的
     pthread_cancel(receiveThread);
     pthread_join(receiveThread,NULL);
     printf("after thread join\n");
     Close(client);
     return;
+}
+
+void test_for_concurrence(const char *ip,  size_t port, size_t num)
+{
+    printf("num [%lu] , port [%lu]\n", num, port);
+    std::vector<remote_client> clientVec;
+    clientVec.reserve(num);
+    size_t volume = 0;
+    int failConnection = 0;
+    for (size_t i = 0; i < num; ++i)
+    {
+        auto conn = Connect(ip, port);
+        clientVec.push_back(conn);
+        if (conn.clientfd)
+        {
+            volume++;
+            if (volume > 100)
+            {
+                printf("当前连接数量 num = [%d]\n", i);
+                volume = 0;
+            }
+        }
+        else
+        {
+            ++failConnection;
+            printf("something wrong : 连接不成功\n");
+        }
+    }
+
+    printf("连接完成: 失败的连接数量为 [%d]\n", failConnection);
+    std::string cmd;
+    std::string msg = "message from client";
+    while (true)
+    {
+        std::cin >> cmd;
+        if (0 == strncmp(cmd.data(), "over", strlen("over")))
+        {
+            printf("退出...\n");
+            break;
+        }
+        else if(0 == strncmp(cmd.data(),"concurr",strlen("concurr")))
+        {
+            size_t conncurr = 0;
+            std::cout<<"请输入需要并发的数量:";
+            std::cin>>conncurr;
+            if(conncurr>num){
+                continue;
+            }
+            for(size_t i=0;i<conncurr;++i){
+                printf("send msg\n");
+                write(clientVec[i].clientfd, msg.data(), msg.length());
+            }
+            continue;
+        }
+
+        try
+        {
+            size_t selectClient = atoi(cmd.data());
+            printf("select Cliet [%lu] clientVec.size() = [%lu]\n", selectClient, clientVec.size());
+            if (selectClient < clientVec.size())
+            {
+                printf("send msg\n");
+                write(clientVec[selectClient].clientfd, msg.data(), msg.length());
+            }
+        }
+        catch (...)
+        {
+            printf("atoi fail\n");
+        }
+    }
+
+    for (size_t i = 0; i < clientVec.size(); ++i)
+    {
+        Close(clientVec[i]);
+    }
+    printf("test for concurrency end\n");
 }
--- main/main.cpp
+++ main/main.cpp
@@ -9,97 +9,17 @@
 #include <stdint.h>
 #include <string.h> // bzero
 #include <stdlib.h>
-
 #include "shm_mutex.h"
-
 #include "../common/nginx/nginx_log.h"
 #include "../common/net/net_tool.h"
-
 #include <thread>
+
 using namespace NET;
 
 // shm_mutex_t process_mutex;
 
-
-
-void test_for_concurrence(const char *ip, size_t num, size_t port)
-{
-    printf("num [%d] , port [%d]\n", num, port);
-    std::vector<remote_client> clientVec;
-    clientVec.reserve(num);
-    static size_t volume = 0;
-
-    int failConnection = 0;
-    for (size_t i = 0; i < num; ++i)
-    {
-        auto conn = Connect(ip, port);
-        clientVec.push_back(conn);
-        if (conn.clientfd)
-        {
-            volume++;
-            if (volume > 100)
-            {
-                printf("当前连接数量 num = [%d]\n", i);
-                volume = 0;
-            }
-        }
-        else
-        {
-            ++failConnection;
-            printf("something wrong : 连接不成功\n");
-        }
-    }
-
-    printf("连接完成: 失败的连接数量为 [%d]\n", failConnection);
-    std::string cmd;
-    std::string msg = "message from client";
-    while (true)
-    {
-        std::cin >> cmd;
-        if (0 == strncmp(cmd.data(), "over", strlen("over")))
-        {
-            printf("退出...\n");
-            break;
-        }
-        else if(0 == strncmp(cmd.data(),"concurr",strlen("concurr")))
-        {
-            size_t conncurr = 0;
-            std::cout<<"请输入需要并发的数量:";
-            std::cin>>conncurr;
-            if(conncurr>num){
-                continue;
-            }
-            for(size_t i=0;i<conncurr;++i){
-                printf("send msg\n");
-                write(clientVec[i].clientfd, msg.data(), msg.length());
-            }
-            continue;
-        }
-
-        try
-        {
-            size_t selectClient = atoi(cmd.data());
-            printf("select Cliet [%lu] clientVec.size() = [%lu]\n", selectClient, clientVec.size());
-            if (selectClient < clientVec.size())
-            {
-                printf("send msg\n");
-                write(clientVec[selectClient].clientfd, msg.data(), msg.length());
-            }
-        }
-        catch (...)
-        {
-            printf("atoi fail\n");
-        }
-    }
-
-    for (size_t i = 0; i < clientVec.size(); ++i)
-    {
-        Close(clientVec[i]);
-    }
-    printf("test for concurrency end\n");
-}
-
 extern void client_to_server();
+extern void test_for_concurrence(const char *ip,  size_t port, size_t num);
 
 int main(int argc, char *argv[])
 {
@@ -107,8 +27,6 @@ int main(int argc, char *argv[])
     //     printf("必须输入两个参数\n");
     //     return 1;
     // }
-
-    // client_to_server(argv);
     // test_for_concurrence(argv[1], atoi(argv[2]), 12345);
 
     client_to_server();
